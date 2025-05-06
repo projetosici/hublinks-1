439bd7428d0c1b956d0fe8e675d6d42d77d5e9fc
Alexsandro Ávila <57649224+AlexVovo@users.noreply.github.com>
2025-05-06 14:56:14 -0300

    Atualizações

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,14 @@ class ProjectHubApp extends StatelessWidget {
 class ProjectHubPage extends StatelessWidget {
   final List<Project> projects = [
     Project(name: 'App de Registro', url: 'https://sisop-rs.web.app/', icon: Icons.assignment),
+    Project(name: 'Documentação App de Registro', url: 'https://alexvovo.github.io/Documentacao-App-Registro/', icon: Icons.assignment),
     Project(name: 'Dash Satisfação ICI', url: 'https://dashsastifacaoici.streamlit.app/', icon: Icons.insert_chart),
     Project(name: 'Teleoncoped', url: 'https://teleoncoped.ici.ong/', icon: Icons.medical_services),
     Project(name: 'Dash Oncológico', url: 'https://dashoncologico.streamlit.app/Painel_Oncologico_Pediatrico_(DATASUS)', icon: Icons.pie_chart),
     Project(name: 'Produção Web', url: 'https://producaoiciweb.web.app/', icon: Icons.code),
     Project(name: 'Page Upload ICI', url: 'http://pageuploud.web.app', icon: Icons.cloud_upload),
     Project(name: 'Page Triagem Oncologia Pediatrica', url: 'https://triagemoncopediatrica.web.app/', icon: Icons.health_and_safety),
+   
   ];
 
   @override
