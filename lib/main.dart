a8766cb243e54b863b84e761a8d3e4d5848c89b5
Alexsandro Ávila <57649224+AlexVovo@users.noreply.github.com>
2025-09-12 20:52:26 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class ProjectHubPage extends StatelessWidget {
     ),
     Project(
       name: 'Documentação App de Registro',
-      url: 'https://projetosici.github.io/Documentacao-App-Registro/',
+      url: 'https://projetobioinfo-pixel.github.io/documentacaoTriagemOnco/',
 
       icon: Icons.assignment,
     ),
